ba32c86e34d974c161908665eae6ddd1b78d4bf6
Ashir Imran <71806327+xlinkfrostx@users.noreply.github.com>
2022-04-20 22:29:21 -0400

    misc

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import express from "express";
-import logger from "morgan";
+// import logger from "morgan";
 import userRouter from "./routes/users.js";
 import postRouter from "./routes/posts.js";
 
 const app = express();
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 
 app.use(express.json());
 
