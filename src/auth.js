97b5996add85dc358fe57113b8ff60eea93ceac9
Joseph Petrillo <jpetrillo119@gmail.com>
2022-05-06 19:28:10 -0400

    feat: start auth

---
+++ src/auth.js
@@ -0,0 +1,54 @@
+import passport from "passport";
+import passportLocal from "passport-local";
+import * as utils from "./dbUtils/userUtils.js";
+
+const { Strategy } = passportLocal;
+
+// Passport Configuration
+// Create a new LocalStrategy object to handle authentication using email and
+// password credentials from the client. The LocalStrategy object is used to
+// authenticate a user using a email and password.
+const strategy = new Strategy(async (email, password, done) => {
+  const user = await utils.getUserByEmail(email);
+  if (!user) {
+    // no such user
+    return done(null, false, { message: "Wrong email" });
+  }
+  if (password !== user.password) {
+    // invalid passwordpassword
+    // should disable logins after N messages
+    // delay return to rate-limit brute-force attacks
+    await new Promise((r) => setTimeout(r, 2000)); // two second delay
+    return done(null, false, { message: "Wrong password" });
+  }
+  // success!
+  // should create a user object here, associated with a unique identifier
+  return done(null, user);
+});
+
+// Configure passport to use the LocalStrategy object.
+// The LocalStrategy object is used to authenticate a user using a email and
+// password. There are other strategies available, but this is the simplest.
+passport.use(strategy);
+
+// Convert user object to a unique identifier.
+passport.serializeUser((user, done) => {
+  done(null, user.userId);
+});
+
+// Convert a unique identifier to a user object.
+passport.deserializeUser(async (userId, done) => {
+  const user = await utils.getUser(userId);
+  done(null, user);
+});
+
+export default {
+  configure: (app) => {
+    app.use(passport.initialize());
+    app.use(passport.session());
+  },
+
+  authenticate: (domain, where) => {
+    return passport.authenticate(domain, where);
+  },
+};
--- src/server.js
+++ src/server.js
@@ -2,27 +2,52 @@ import express from "express";
 import userRouter from "./routes/users.js";
 import postRouter from "./routes/posts.js";
 
+import expressSession from "express-session";
+import auth from "./auth.js";
+
+// Session configuration
+const sessionConfig = {
+  // set this encryption key in Heroku config (never in GitHub)!
+  secret: process.env.SECRET || "SECRET",
+  resave: false,
+  saveUninitialized: false,
+};
+
+// Our own middleware to check if the user is authenticated
+function checkLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+}
+
 const app = express();
 
+app.use(expressSession(sessionConfig));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+auth.configure(app);
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 
-app.use("/beat/", express.static("./src/client/beat"));
+app.use("/beat/", checkLoggedIn, express.static("./src/client/beat"));
 app.get("/beat/:postId", (req, res) => {
   res.sendFile("./src/client/beat/", { root: "./" });
 });
 
-app.use("/song/", express.static("./src/client/song"));
+app.use("/song/", checkLoggedIn, express.static("./src/client/song"));
 app.get("/song/:postId", (req, res) => {
   res.sendFile("./src/client/song/", { root: "./" });
 });
 
-app.use("/profile/", express.static("./src/client/profile"));
+app.use("/profile/", checkLoggedIn, express.static("./src/client/profile"));
 app.get("/profile/:userId", (req, res) => {
   res.sendFile("./src/client/profile/", { root: "./" });
 });
+
+app.use("/login/", express.static("./src/client/login"));
 
 app.use(express.static("./src/client"));
 
